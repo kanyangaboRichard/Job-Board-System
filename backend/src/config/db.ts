5b354a4dcf200320d0a6b7f31c4eb6104a710339
Richard <iirisi454@gmail.com>
2025-11-04 08:06:47 +0200

    added Render

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,14 +2,22 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new pg.Pool({
+const connectionConfig = isProduction
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {rejectUnauthorized: false},
+    }
+  : {
+
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
+};
+const pool = new pg.Pool(connectionConfig);
 
 export const connectDB = async () => {
   try {
