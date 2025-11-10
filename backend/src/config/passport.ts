2ab3f9206e10b1b319b8f22f1aad3a9b0b0b48d9
Richard <iirisi454@gmail.com>
2025-11-10 15:34:47 +0200

    editted process.env

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -51,8 +51,8 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       callbackURL:
         process.env.NODE_ENV === "production"
-          ? "https://localhost:3005/api/auth/google/callback"
-          : "https://job-board-system-silk.vercel.app/api/auth/google/callback",
+          ? "https://job-board-system-silk.vercel.app/api/auth/google/callback"
+          : "http://localhost:3005/api/auth/google/callback",
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
